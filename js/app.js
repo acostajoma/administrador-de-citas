28a20b35618f42bef558884aca90b9b8d67bf8e2
Jose Acosta <acostajoma9@gmail.com>
2022-06-16 07:56:10 -0600

    Cambiar el scripting del div de citas a una solucion utilizando un forEach

--- js/app.js
+++ js/app.js
@@ -61,53 +61,30 @@ class UI {
             divCita.dataset.id = id;
 
             // Scripting de los elementos de la cita
-            const mascotaParrafo = document.createElement('h2');
-            mascotaParrafo.classList.add('card-title', 'font-weight-bolder');
-            mascotaParrafo.textContent = mascota;
+            Object.entries(cita).forEach(([key, value]) => {
+                if (key === 'mascota'){
+                    const mascotaParrafo = document.createElement('h2');
+                    mascotaParrafo.classList.add('card-title', 'font-weight-bolder');
+                    mascotaParrafo.textContent = value;
+                    divCita.appendChild(mascotaParrafo);
 
-            const propietarioParrafo = document.createElement('p');
-            propietarioParrafo.innerHTML = `
-                <span class="font-weight-bolder">Propietario:</span> ${propietario}
-            `;
+                } else {
+                    const parrafo = document.createElement('p');
+                    parrafo.innerHTML = `
+                        <span class="font-weight-bolder">Propietario:</span> ${value}
+                    `;
+                    divCita.appendChild(parrafo);
 
-            const telefonoParrafo = document.createElement('p');
-            telefonoParrafo.innerHTML = `
-                <span class="font-weight-bolder">Teléfono:</span> ${telefono}
-            `;
-
-            const fechaParrafo = document.createElement('p');
-            fechaParrafo.innerHTML = `
-                <span class="font-weight-bolder">Fecha:</span> ${fecha}
-            `;
-
-            const horaParrafo = document.createElement('p');
-            horaParrafo.innerHTML = `
-                <span class="font-weight-bolder">Hora:</span> ${hora}
-            `;
-
-            const sintomasParrafo = document.createElement('p');
-            sintomasParrafo.innerHTML = `
-                <span class="font-weight-bolder">Sintomas:</span> ${sintomas}
-            `;
+                }
+            });
 
             // Boton para eliminar cita
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
             btnEliminar.innerHTML = `Eliminar <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>`
-
-            btnEliminar.onclick = () => eliminarCita(id);
-
-            // Agregar los parrafos al div
-            divCita.appendChild(mascotaParrafo);
-            divCita.appendChild(propietarioParrafo);
-            divCita.appendChild(telefonoParrafo);
-            divCita.appendChild(fechaParrafo);
-            divCita.appendChild(horaParrafo);
-            divCita.appendChild(sintomasParrafo);
-            divCita.appendChild(btnEliminar);
-
-
+            btnEliminar.onclick = () => eliminarCita(id); //Funcion del boton
+            divCita.appendChild(btnEliminar); //agrega al div
 
             // Agregar al HTML
             contenedorCitas.appendChild(divCita);
